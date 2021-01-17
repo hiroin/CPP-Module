f5261f48fba171ffb24b985689fdeb7a642452a4
hiroin <egamescape@gmail.com>
2021-01-17 08:40:00 +0100

    Fix to ex04 of the CPP module 01

--- 01/ex04/ex04.cpp
+++ 01/ex04/ex04.cpp
@@ -6,15 +6,30 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 08:15:46 by user42            #+#    #+#             */
-/*   Updated: 2020/11/27 12:13:10 by user42           ###   ########.fr       */
+/*   Updated: 2021/01/17 08:37:26 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
 
+void	changePointer(int* i)
+{
+	*i = 0;	
+}
+
+void	change(int& i)
+{
+	i = 0;	
+}
+
 int main(void)
 {
+	int	i = 42;
+	change(i);
+	std::cout << i << std::endl;
+	std::cout << std::endl;
+
 	char	s[17] = "HI THIS IS BRAIN";
 	char	*p = s;
 	char&	r = *s;
@@ -23,6 +38,7 @@ int main(void)
 	std::cout << p << std::endl;
 	std::cout << "reference" << std::endl;
 	std::cout << &r << std::endl;
+	std::cout << std::endl;
 
 	std::string	string = "HI THIS IS BRAIN";
 	std::string	*pstring = &string;
@@ -32,6 +48,7 @@ int main(void)
 	std::cout << *pstring << std::endl;
 	std::cout << "reference" << std::endl;
 	std::cout << rstring << std::endl;
+	std::cout << std::endl;
 
 	return (0);
 }
